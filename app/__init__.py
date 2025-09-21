26c937814a37954856ba09b13bae954ca7a431ef
Adham El-Sheikh <aelsheikh@sumerge.com>
2025-09-21 15:52:25 +0300

    'changes'

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
-
 from flask import Flask
+from app.routes import main_blueprint
 from app.routes.user_routes import user_blueprint
 from app.routes.product_routes import product_blueprint
 
@@ -7,6 +7,7 @@ def create_app():
     app = Flask(__name__)
 
     # Register blueprints
+    app.register_blueprint(main_blueprint)
     app.register_blueprint(user_blueprint)
     app.register_blueprint(product_blueprint)
 
